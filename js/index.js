20d0b15f585484322be8376d62f55a68f1e02b31
Veronica Dang <verooodang@gmail.com>
2020-02-18 19:12:48 -0800

    tuesday

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$("#headingOne").on("click", function(){
+    $("#collapseOne").slideToggle();
+
+})
+
+$("#headingTwo").on("click", function(){
+    $("#collapseTwo").slideToggle();
+
+})
+
+$("#headingThree").on("click", function(){
+    $("#collapseThree").slideToggle();
+
+})
+
+#usertest, #sitemap, #wireframe {
+    width: 80%;
+}
+
